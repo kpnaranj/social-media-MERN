eb2374a17fe0b02004f812b4145af40d62996ca5
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-05-26 13:43:32 -0400

    Succesfully connected server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
 // Private libraries
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 // Public libraries
 require("dotenv").config();
 const PORT = process.env.PORT;
+const db = process.env.MONGO_URI;
+// MongoDB database
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+mongoose.connection
+  .on("error", () => {
+    console.log("We are not connected to the server");
+  })
+  .on("connected", () => {
+    console.log(`We are connected to the server ${db}`);
+  });
 // Middlewares
 const customMiddleWare = (req, res, next) => {
   console.log("this is a middleware");
@@ -13,7 +28,7 @@ app.use(customMiddleWare);
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
-app.get("/home",customMiddleWare, (req, res) => {
+app.get("/home", customMiddleWare, (req, res) => {
   res.send("Hello World from Home");
 });
 // Listening port server
